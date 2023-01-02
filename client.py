de4ff197861a3ffebab57914a87a69eaa774f6e8
Eduardo Freire <dudufreiresantos@gmail.com>
2023-01-02 16:06:35 -0300

    implement beginning of privmsg

--- client.py
+++ client.py
@@ -1,8 +1,29 @@
 import socket
+import threading
 import constants
+from parse import *
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((constants.serverAddress, constants.serverPort))
+
+def listenMessages():
+	while True:
+		try:
+			handleMessage(clientSocket.recv(constants.bufsize).decode())
+		except:
+			break
+
+def executePrivmsg(cmd: Command):
+	print(cmd.params["text"])
+
+def handleMessage(msg: str):
+	cmd = parseMessage(msg)
+
+	if cmd.cmdType == CmdType.PRIVMSG: executePrivmsg(cmd)
+
+
+messageThread = threading.Thread(target=listenMessages, daemon=True)
+messageThread.start()
 
 while True:
 	msg = input()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import queue
 import constants
+from parse import *
 
 
 class User:
@@ -43,7 +44,7 @@ def handleUser(user: User):
 	if debug: print(f"Handling user {user.username}.")
 	while True:
 		try:
-			msgQueue.put((user.addr, user.userSocket.recv(constants.bufsize)))
+			msgQueue.put((user.addr, user.userSocket.recv(constants.bufsize).decode()))
 		except:
 			break
 
@@ -51,16 +52,44 @@ def handleUser(user: User):
 acceptThread = threading.Thread(target=acceptClient, daemon=True)
 acceptThread.start()
 
+# TODO : Check if username and realname are well-formed.
+def executeUser(addr, cmd: Command):
+	users[addr].username = cmd.params["username"]
+	users[addr].realname = cmd.params["realname"]
+
+# TODO : Check if nick is well-formed.
+def executeNick(addr, cmd: Command):
+	users[addr].username = cmd.params["nickname"]
+
+def executePrivmsg(addr, cmd: Command):
+	target = cmd.params["target"]
+	text = cmd.params["text"]
+
+	if target[0] == "#":
+		return
+
+	for addrTarget in users:
+		if users[addrTarget].username == target:
+			msg = f"PRIVMSG {target} :{users[addr].username}: {text}"
+			users[addrTarget].userSocket.send(msg.encode())
+
+			return
+
+	# TODO : Handle case where target was not found.
+	
+	
+
+
+
+def handleMessage(msgPair) -> None:
+	addr, msg = msgPair
+	cmd = parseMessage(msg)
+
+	if cmd.cmdType == CmdType.USER: executeUser(addr, cmd)
+	elif cmd.cmdType == CmdType.NICK: executeNick(addr, cmd)
+	elif cmd.cmdType == CmdType.PRIVMSG: executePrivmsg(addr, cmd)
 
 while True:
 	if msgQueue.empty(): continue
 
-	msg = msgQueue.get()[1].decode()
-	if msg == "exit":
-		for user in users:
-			user.userThread.close()
-		
-		serverSocket.close()
-		break
-
-	print(msg)
+	handleMessage(msgQueue.get())

f837a32e7da528564dc6a7bf93309f0422dd6946
Eduardo Freire <dudufreiresantos@gmail.com>
2023-01-02 15:17:24 -0300

    implement user class

--- server.py
+++ server.py
@@ -3,6 +3,16 @@ import threading
 import queue
 import constants
 
+
+class User:
+	def __init__(self, userSocket: socket, addr) -> None:
+		self.userSocket = userSocket
+		self.username = f"guest{len(users) + 1}"
+		self.realname = ""
+		self.addr = addr
+		self.userThread = None
+
+
 debug = True
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,29 +20,30 @@ serverSocket.bind(('', constants.serverPort))
 
 serverSocket.listen()
 
-clients = {}
+users = {}
 
 msgQueue = queue.Queue()
 
-clientThreads = {}
 def acceptClient():
 	while True:
 		try:
 			connectionSocket, addr = serverSocket.accept()
 			if debug: print(f"Accepted connection from {addr}.")
 
-			clients[addr] = connectionSocket
-			clientThreads[addr] = \
-				threading.Thread(target=handleClient, args=(clients[addr],), daemon=True)
-			clientThreads[addr].start()
+			newUser = User(connectionSocket, addr)
+			userThread = threading.Thread(target=handleUser, args=(newUser, ), daemon=True)
+			newUser.userThread = userThread
+			users[addr] = newUser
+			newUser.userThread.start()
+
 		except OSError:
 			break
 
-def handleClient(connectionSocket: socket):
-	if debug: print(f"Handling client {connectionSocket.getsockname()}.")
+def handleUser(user: User):
+	if debug: print(f"Handling user {user.username}.")
 	while True:
 		try:
-			msgQueue.put(connectionSocket.recv(constants.bufsize))
+			msgQueue.put((user.addr, user.userSocket.recv(constants.bufsize)))
 		except:
 			break
 
@@ -44,10 +55,10 @@ acceptThread.start()
 while True:
 	if msgQueue.empty(): continue
 
-	msg = msgQueue.get().decode()
+	msg = msgQueue.get()[1].decode()
 	if msg == "exit":
-		for client in clients:
-			client.close()
+		for user in users:
+			user.userThread.close()
 		
 		serverSocket.close()
 		break
